53437284fdefa8cf809fe7f123c357ed35d8db24
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2024-01-18 16:52:28 +0100

    Created the components

---
+++ Nike/src/components/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+
+
+export {
+  Hero,
+  Footer,
+  Services,
+  PopularProducts,
+  SuperQuality,
+  Subscribe,
+  SpecialOffer,
+  CustomerReviews,
+};
